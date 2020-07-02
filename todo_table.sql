20bd8d8cdbcd11ae236496d840cd072faddfae16
57080110 <satoshi.okazaki0812@gmail.com>
2020-07-02 22:23:28 +0900

    first commit

---
+++ todo_table.sql
@@ -0,0 +1,70 @@
+﻿-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- ホスト: 127.0.0.1
+-- 生成日時: 2020 年 7 朁E02 日 15:18
+-- サーバのバージョン： 10.4.13-MariaDB
+-- PHP のバージョン: 7.4.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- データベース: `gsacf_db06_06`
+--
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `todo_table`
+--
+
+CREATE TABLE `todo_table` (
+  `id` int(12) NOT NULL,
+  `todo` varchar(128) COLLATE utf8_unicode_ci NOT NULL,
+  `deadline` date NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- テーブルのデータのダンプ `todo_table`
+--
+
+INSERT INTO `todo_table` (`id`, `todo`, `deadline`, `created_at`, `updated`) VALUES
+(20, 'あいうえお', '2020-07-01', '2020-06-27 15:48:52', '2020-07-02 10:24:12'),
+(21, 'かきくけこ', '2020-07-01', '2020-06-27 16:47:24', '2020-07-02 10:24:22'),
+(22, 'さしすせそ', '2020-07-01', '2020-07-01 23:05:50', '2020-07-02 10:24:34');
+
+--
+-- ダンプしたテーブルのインデックス
+--
+
+--
+-- テーブルのインデックス `todo_table`
+--
+ALTER TABLE `todo_table`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- ダンプしたテーブルのAUTO_INCREMENT
+--
+
+--
+-- テーブルのAUTO_INCREMENT `todo_table`
+--
+ALTER TABLE `todo_table`
+  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
